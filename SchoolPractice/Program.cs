b910a49a691ccbf5058fed067118397d06d3be67
Kara Gruenewald <karakilkenny@karas-imac.attlocal.net>
2020-07-13 11:58:31 -0500

    completed

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,27 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Test your exercise solutions with print statements here.
+
+            Student student1 = new Student("Kara", 100, 0, 3.0);
+            Student student2 = new Student("Kara", 100);
+
+            Console.WriteLine(student1.GetGradeLevel(student1.NumberOfCredits));
+
+            student1.AddGrade(3, 4.0);
+            Console.WriteLine(student1.Gpa);
+            Console.WriteLine(student1.NumberOfCredits);
+
+            student1.AddGrade(4, 3.5);
+            Console.WriteLine(student1.Gpa);
+            Console.WriteLine(student1.NumberOfCredits);
+
+            Console.WriteLine(student1.ToString());
+
+            Course testCourse = new Course();
+
+            Console.WriteLine(student1.Equals(student2));
+
+            Console.WriteLine(student1.Equals(testCourse));
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -30,21 +30,71 @@ namespace SchoolPractice
         // TODO: Complete the AddGrade method.
         public void AddGrade(int courseCredits, double grade)
         {
+            double qualityScore = courseCredits * grade;
+
+            double totalQualityScore = (this.Gpa * this.NumberOfCredits) + qualityScore;
+
+            int newCreditTotal = this.NumberOfCredits + courseCredits;
+            this.Gpa = totalQualityScore / newCreditTotal;
+            this.NumberOfCredits = newCreditTotal;
+
+
             // Update the appropriate properties: NumberOfCredits, Gpa
         }
 
         //TODO: Complete the GetGradeLevel method here:
         public string GetGradeLevel(int credits)
         {
-            // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
+            
+            if (credits >= 90)
+            {
+                return "Senior";
+            }
+            else if (credits >= 60)
+            {
+                return "Junior";
+            }
+            else if (credits >= 30)
+            {
+                return "Sophmore";
+            }
+            else
+            {
+                return "Freshman";
+            }
+
         }
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
         //  than just the class fields.
 
+        public override string ToString()
+        {
+            string output = "Student info:\n Name: " + this.Name + "\n GPA: " + this.Gpa + "\n ID: " + this.StudentId + "\n Credits: " + this.NumberOfCredits;
+            return output;
+                }
+
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Student objects equal.
 
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            Student toCompare = obj as Student;
+
+            return this.Name == toCompare.Name && this.StudentId == toCompare.StudentId;
+        }
     }
 }
